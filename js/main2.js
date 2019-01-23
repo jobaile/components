ef8a405cd7c4e5bbce22c695f02ace4b75ef0464
Joanna Baile <joanna@studentsofferingsupport.org>
2019-01-23 12:09:55 -0500

    routing UI edits

--- js/main2.js
+++ js/main2.js
@@ -2,20 +2,25 @@
 //components will go here
 const HomePageComponent = {
     template: "<h2>You're on the home page</h2>" 
-    };
+};
 
 const UsersPageComponent = {
     template: "<h2>You're on the users page</h2>"
-    };
+};
 
 const ContactPageComponent = {
     template: "<h2>You're on the contact page</h2>"
 };
 
+const ErrorPageComponent = {
+    template: "<h2>Page Not Found! Please try again.</h2>" 
+};
+
 const routes = [
     { path: '/', name: 'home', component: HomePageComponent },
-    { path: '/users', name: 'users', component: UsersPageComponent}
-    { path: '/contact', name: 'contact', component: UsersPageComponent}
+    { path: '/users', name: 'users', component: UsersPageComponent},
+    { path: '/contact', name: 'contact', component: ContactPageComponent},
+    { path: '/*', name: 'error', component: ErrorPageComponent}
 ];
 
 const router = new VueRouter ({
