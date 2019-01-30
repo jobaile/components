74c3773473147df4df10edf05794ca2289a85278
Joanna Baile <joanna@studentsofferingsupport.org>
2019-01-30 11:27:04 -0500

    components stuff

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# components_week2
+# Using VUE Router
+
+##Important Notes
+Two different ways to do the routing:
+* <router-link :to="{ path: '/'}" class="link">Home</router-link>
+* <a href="#/contact">Contact</a>
--- js/main2.js
+++ js/main2.js
@@ -5,7 +5,37 @@ const HomePageComponent = {
 };
 
 const UsersPageComponent = {
-    template: "<h2>You're on the users page</h2>"
+    //template: "<h2>You're on the users page</h2>"
+    props: ['id'],
+    template: "#userList",
+
+    //this always needs to be a function in a component
+    data: function(){
+        return {
+            users: []
+        }
+    },
+
+    created: function(){
+        console.log('user component created!');
+
+        this.fetchUserData(this.id);
+    },
+
+    methods: {
+        fetchUserData(user){
+            debugger;
+
+            let url = `./includes/index.php?user=${user}`;
+
+            fetch(url)
+                .then(res => res.json())
+                .then(data => this.users = data)
+            .catch(function(error){
+                console.error(error);
+            });
+        }
+    }
 };
 
 const ContactPageComponent = {
@@ -18,7 +48,7 @@ const ErrorPageComponent = {
 
 const routes = [
     { path: '/', name: 'home', component: HomePageComponent },
-    { path: '/users', name: 'users', component: UsersPageComponent},
+    { path: '/users/:id', name: 'users', component: UsersPageComponent, props: true },
     { path: '/contact', name: 'contact', component: ContactPageComponent},
     { path: '/*', name: 'error', component: ErrorPageComponent}
 ];
